c0efc19ea0aafb983d069fd6ea0a01a0c2fda54a
LuchoLesca <lescaluciano@hotmail.com>
2025-09-03 13:01:13 -0300

    feat: add dynamically the highlighted services

---
+++ src/consts/servicesHighlights.ts
@@ -0,0 +1,17 @@
+export const SERVICE_HIGHLIGHTS = [
+  {
+    icon: { name: 'award', styles: 'text-blue-600' },
+    text1: '+5',
+    text2: 'Años de experiencia',
+  },
+  {
+    icon: { name: 'clock', styles: 'text-green-600' },
+    text1: '24-48h',
+    text2: 'Tiempo promedio',
+  },
+  {
+    icon: { name: 'star', styles: 'text-yellow-500' },
+    text1: '100%',
+    text2: 'Garantizado',
+  },
+];
